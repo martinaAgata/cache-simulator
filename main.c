f805216b5309cacb447b78aaa9845aa96aed6974
MatRH <ruizhuidobro.matias@gmail.com>
2020-07-16 20:31:08 -0300

    Corregido

--- main.c
+++ main.c
@@ -39,21 +39,4 @@ int main(int argc, char const *argv[]) {
 }
 
 int cache_simulator(FILE* tracefile, cache_t* cache){
-
-
-int main(int argc, char const *argv[]) {
-	// ./cachesim tracefile.xex C E S [ -v n m ]
-	// el archivo de traza a simular
-	// el tamaño de la caché C, en bytes
-	// la asociatividad de la caché, E
-	// el número de sets de la caché, S
-
-	/* Condiciones de error
-	si el número de argumentos no es 4 o 7;
-	si el archivo de trazas especificado no existe;
-	si alguno de los parámetros C, E o S no son potencia de dos;
-	si los argumentos n y m del modo verboso no son números enteros que cumplan 0 ≤ n ≤ m
-	*/
-	cache();
-	return 0;
 }
