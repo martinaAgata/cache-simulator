91de06b47bf3c3af8fa90c897969bdded2f7bd53
MatRH <ruizhuidobro.matias@gmail.com>
2020-07-16 20:14:54 -0300

    Vamos viendo

--- main.c
+++ main.c
@@ -6,9 +6,14 @@
 cache_t *create_cache(size_t size, size_t asoc, size_t sets) {
 }
 
-int tracefile_parser() {
-}
 
+/* Valida que el input sea correcto
+    Abre el archivo
+    Crea el cache
+    Llama al simulador
+    Cierra el archivo
+    Libera el cache
+*/
 int main(int argc, char const *argv[]) {
 	// ./cachesim tracefile.xex C E S [ -v n m ]
 	// el archivo de traza a simular
@@ -22,6 +27,25 @@ int main(int argc, char const *argv[]) {
 	si alguno de los parámetros C, E o S no son potencia de dos;
 	si los argumentos n y m del modo verboso no son números enteros que cumplan 0 ≤ n ≤ m
 	*/
-	cache();
+
+  //Tomo la ruta relativa o completa del primer archivo pasado
+  const char* file_path = argv[1];
+  //Lo abro en modo lectura, guardando el file_descriptor
+  int trace_file = open(file_path, O_RDONLY);
+  //Finalizo la ejecución si no falló al abrir el archivo
+  if (trace_file == -1){
+    if (strcmp("EISDIR", strerror(errno)) == 0){
+      fprintf( stderr, "Error: el archivo '%s' es un directorio\n", file_path);
+      return 3;
+    }
+    fprintf( stderr, "Error: archivo '%s' inaccesible\n", file_path);
+    return 2;
+  }
+	cache_simulator(trace_file, cache);
 	return 0;
+}
+
+int cache_simulator(char const* filename, cache_t* cache){
+
+
 }
