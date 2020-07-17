b0f9c918329dac346aec1602fcdbd5e45d4eae1b
MartinaAgata <mpanetta@fi.uba.ar>
2020-07-16 23:17:26 -0300

    Añado versión provisoria de structs y programas de manejo de strings

--- main.c
+++ main.c
@@ -1,9 +1,43 @@
 #include <stdlib.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-// incluir clase
+#include "strutil.h"
 
-cache_t *create_cache(size_t size, size_t asoc, size_t sets) {
+/********************* Estructuras provisorias *********************/
+
+typedef struct {
+	int c;
+	int e;
+	int s;
+	int block_bytes;
+} cache_t;
+
+typedef struct {
+	int valid;
+	int dirty;
+	int tag;
+	int block;
+} line_t;
+
+typedef struct {
+	line_t* lines;
+	int size; // size = E
+} set_t;
+
+/*******************************************************************/
+
+
+int cache_read(cache_t *cache, int memory_address, size_t bytes_amount, int *data) {
+
+}
+
+int cache_write(cache_t *cache, int memory_address, size_t bytes_amount, int *data) {
+
+}
+
+
+cache_t *create_cache(size_t size, size_t e, size_t s) {
 }
 
 /* Valida que el input sea correcto
@@ -13,36 +47,9 @@ cache_t *create_cache(size_t size, size_t asoc, size_t sets) {
 	Cierra el archivo
 	Libera el cache
 */
-int number_is_valid(unsigned int x) {
-	return x && !(x & (x - 1));
-}
 
-int main(int argc, char const *argv[]) {
-	// ./cachesim tracefile.xex C E S [ -v n m ]
-	if (argc != 4 || argc != 7) {
-		fprintf(stderr, "Cantidad de argumentos incorrecta\n");
-		exit(1);
-	}
-	int c = atoi(argv[1]); // el tamaño de la caché C, en bytes
-	int e = atoi(argv[2]); // la asociatividad de la caché, E
-	int s = atoi(argv[3]); // el número de sets de la caché, S
-	if (c <= 0 || e <= 0 || s <= 0) { // FALTA CHEQUEAR SI ADEMAS SON POWER OF 2
-		fprintf(stderr, "Argumento incorrecto\n");
-		exit(1);
-	}
 
-	FILE* tracefile;
-	const char* input_file = argv[1];
-	tracefile = fopen(input_file, "r");
-	if (!tracefile) {
-		fprintf(stderr, "Error: archivo fuente inaccesible");
-		return 0;
-	}
-	cache_simulator(tracefile, cache);
-	return 0;
-}
-
-int cache_simulator(FILE* tracefile, cache_t* cache){
+int cache_simulator(FILE* tracefile, cache_t* cache) {
 	// Parseo las líneas del archivo
 	int instruction_pointer, memory_address, data;
 	char operation;
@@ -53,13 +60,13 @@ int cache_simulator(FILE* tracefile, cache_t* cache){
 	char* line = NULL; // Para que getline se encargue de manejar la memoria
 	size_t bytes = 0; // Para que getline se encargue de manejar la memoria
 	long int read_bytes;
-
+	FILE* input;
 	while ((read_bytes = getline(&line, &bytes, input)) != -1 && !errors) {
 		if (line[read_bytes - 1] == '\n') line[--read_bytes] = '\0'; // Piso el \n
 		strv = split(line, ' '); // Separo la línea por espacios
 
 		instruction_pointer = (int) strtol(strv[0], NULL, 16);
-		operation = strv[1];
+		operation = strv[1][0];
 		memory_address = (int) strtol(strv[2], NULL, 16);
 		bytes_amount = (size_t) atoi(strv[3]);
 		data = (int) strtol(strv[4], NULL, 16);
@@ -75,4 +82,40 @@ int cache_simulator(FILE* tracefile, cache_t* cache){
 	}
 	free(line);
 	if (!errors) return 0;
+}
+
+
+int is_power_of_2(int x) {
+	return x && !(x & (x - 1));
+}
+
+int inputs_are_valid(int a, int b, int c) {
+	if (a <= 0 || b <= 0 || c <= 0) return 0;
+	return is_power_of_2(a) || is_power_of_2(b) || is_power_of_2(c);
+}
+
+int main(int argc, char const *argv[]) {
+	// ./cachesim tracefile.xex C E S [ -v n m ]
+	if (!(argc == 5 || argc == 8)) {
+		fprintf(stderr, "Error: Cantidad de argumentos incorrecta\n");
+		exit(1);
+	}
+	int c = atoi(argv[1]); // tamaño de la caché C (bytes)
+	int e = atoi(argv[2]); // número de líneas por set
+	int s = atoi(argv[3]); // número de sets
+	if (inputs_are_valid(c,e,s)) {
+		fprintf(stderr, "Error: Argumento incorrecto\n");
+		exit(1);
+	}
+
+	FILE* tracefile;
+	const char* input_file = argv[1];
+	tracefile = fopen(input_file, "r");
+	if (!tracefile) {
+		fprintf(stderr, "Error: archivo fuente inaccesible");
+		return 0;
+	}
+	cache_t *cache = create_cache(c, e, s);
+	int cache_result = cache_simulator(tracefile, cache);
+	return 0;
 }
---
+++ strutil.c
@@ -0,0 +1,79 @@
+#define _POSIX_C_SOURCE 200809L
+#include "strutil.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void imprimir_strv(char** strv);
+
+void free_strv(char* strv[]) {
+	if(strv==NULL) return;
+	for(int i=0; strv[i]; i++) free(strv[i]);
+	free(strv);
+} 
+
+char** split(const char* str, char sep) {
+	if(str==NULL) return NULL;
+	size_t contador = 2;
+	int i=0;
+	while(str[i]) if(str[i++] == sep) contador++;
+	char** strv = malloc(contador*sizeof(char*));
+	if(!strv) return NULL;
+
+	size_t len = 0;
+	int j=0, k=0;
+	if(!str[j] && !len) {
+		strv[k] = malloc(sizeof(char));
+		if(!strv[k]) return NULL;
+		strv[k++][0] = '\0';
+	} 
+	while (str[j]) {
+		if(str[j] == sep || !str[j+1]) {
+			if(!str[j+1] && str[j] != sep) {
+				len++;
+				j++;
+			}
+			strv[k] = malloc((len+1)*sizeof(char));
+			if (!strv[k]) return NULL;
+			memcpy(strv[k], &str[j-len], len);
+			strv[k++][len] = '\0';
+			len = 0;
+			if(str[j] != '\0' && str[j] == sep && !str[j+1]) {
+				strv[k] = malloc(sizeof(char));
+				if(!strv[k]) return NULL;
+				strv[k++][0] = '\0';
+			}
+		}
+		else len++;
+		if(str[j]) j++;
+	}
+	strv[k] = NULL;
+	return strv;
+}
+
+char* join(char** strv, char sep) {
+	if(strv==NULL) return NULL;
+	if(strv[0]==NULL) return strdup("");
+	size_t totales=0;
+	for(int i=0; strv[i]; i++) totales += (strlen(strv[i])+1);
+	char* cadena = malloc(totales*sizeof(char));
+	if(!cadena) return NULL;
+	size_t longitud_actual=0;
+	for(int i=0; strv[i] != NULL; i++) {
+		strcpy(cadena+longitud_actual, strv[i]);
+		longitud_actual += strlen(strv[i]);
+		if(strv[i+1]) cadena[longitud_actual++] = sep;
+	}
+	return cadena;
+}
+
+void imprimir_strv(char** strv) {
+	if(strv==NULL) return;
+	int j=0;
+	while(strv[j]) {
+		printf("#%s# ", strv[j]);
+		j++;
+	}
+	printf("\n");
+}
+
