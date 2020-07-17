c6d9fe3de64a1fccd81ea524e27cc16b041ba5cf
MartinaAgata <mpanetta@fi.uba.ar>
2020-07-16 21:26:09 -0300

    Corrijo espacios y sumo chequeos de input

--- main.c
+++ main.c
@@ -13,19 +13,23 @@ cache_t *create_cache(size_t size, size_t asoc, size_t sets) {
 	Cierra el archivo
 	Libera el cache
 */
+int number_is_valid(unsigned int x) {
+	return x && !(x & (x - 1));
+}
+
 int main(int argc, char const *argv[]) {
 	// ./cachesim tracefile.xex C E S [ -v n m ]
-	// el archivo de traza a simular
-	// el tamaño de la caché C, en bytes
-	// la asociatividad de la caché, E
-	// el número de sets de la caché, S
-
-	/* Condiciones de error
-	si el número de argumentos no es 4 o 7;
-	si el archivo de trazas especificado no existe;
-	si alguno de los parámetros C, E o S no son potencia de dos;
-	si los argumentos n y m del modo verboso no son números enteros que cumplan 0 ≤ n ≤ m
-	*/
+	if (argc != 4 || argc != 7) {
+		fprintf(stderr, "Cantidad de argumentos incorrecta\n");
+		exit(1);
+	}
+	int c = atoi(argv[1]); // el tamaño de la caché C, en bytes
+	int e = atoi(argv[2]); // la asociatividad de la caché, E
+	int s = atoi(argv[3]); // el número de sets de la caché, S
+	if (c <= 0 || e <= 0 || s <= 0) { // FALTA CHEQUEAR SI ADEMAS SON POWER OF 2
+		fprintf(stderr, "Argumento incorrecto\n");
+		exit(1);
+	}
 
 	FILE* tracefile;
 	const char* input_file = argv[1];
@@ -40,21 +44,19 @@ int main(int argc, char const *argv[]) {
 
 int cache_simulator(FILE* tracefile, cache_t* cache){
 	// Parseo las líneas del archivo
-	int instruction_pointer;
+	int instruction_pointer, memory_address, data;
 	char operation;
-	int memory_address;
 	size_t bytes_amount;
-	int data;
 
 	bool errors = false;
-	char** strv; // para guardar los campos de cada línea
-	char* line = NULL; // para que getline se encargue de manejar la memoria
-	size_t bytes = 0; // para que getline se encargue de manejar la memoria
+	char** strv; // Para guardar los campos de cada línea
+	char* line = NULL; // Para que getline se encargue de manejar la memoria
+	size_t bytes = 0; // Para que getline se encargue de manejar la memoria
 	long int read_bytes;
 
 	while ((read_bytes = getline(&line, &bytes, input)) != -1 && !errors) {
 		if (line[read_bytes - 1] == '\n') line[--read_bytes] = '\0'; // Piso el \n
-		strv = split(line, ' '); // separo la línea por espacios
+		strv = split(line, ' '); // Separo la línea por espacios
 
 		instruction_pointer = (int) strtol(strv[0], NULL, 16);
 		operation = strv[1];
@@ -68,8 +70,8 @@ int cache_simulator(FILE* tracefile, cache_t* cache){
 		else if (operation == 'R') {
 			errors = cache_read(cache, memory_address, bytes_amount, &data);
 		}
-		if (errors) break;
 		free_strv(strv);
+		if (errors) break;
 	}
 	free(line);
 	if (!errors) return 0;
