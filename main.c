c1c941b2684a2c55e6da2acabec8634dc3437857
MartinaAgata <mpanetta@fi.uba.ar>
2020-07-17 02:20:58 -0300

    Añado opción modo verbose

--- main.c
+++ main.c
@@ -4,14 +4,14 @@
 #include <string.h>
 #include "strutil.h"
 
-/********************* Estructuras provisorias *********************/
+// Las direcciones de memoria son siempre de 32 bits.
+// La política que desalojo, que es siempre least-recently used (LRU)
+// La penalty por accesos a memoria es siempre 100 ciclos
 
-typedef struct {
-	int c;
-	int e;
-	int s;
-	int block_bytes;
-} cache_t;
+// #include <stdint.h>
+// typedef uint32_t memory_address; QUIZAS SIRVA
+
+/********************* Estructuras provisorias ********************************/
 
 typedef struct {
 	int valid;
@@ -21,11 +21,19 @@ typedef struct {
 } line_t;
 
 typedef struct {
-	line_t *lines;
+	line_t **lines;
 	int size; // size = E
 } set_t;
 
-/*******************************************************************/
+typedef struct {
+	int c;
+	int e;
+	int s;
+	int block_bytes;
+	set_t **sets;
+} cache_t;
+
+/******************************************************************************/
 
 
 int cache_read(cache_t *cache, int memory_address, size_t bytes_amount, int *data) {
@@ -36,18 +44,25 @@ int cache_write(cache_t *cache, int memory_address, size_t bytes_amount, int *da
 
 }
 
+cache_t *create_cache(size_t c, size_t e, size_t s) {
+	cache_t *cache = malloc(sizeof(cache_t));
+	cache->c = c;
+	cache->e = e;
+	cache->s = s;
+	cache->block_bytes = c / (e*s);
 
-cache_t *create_cache(size_t size, size_t e, size_t s) {
+	for (int i = 0; i < s; i++) {
+		cache->sets[i] = malloc(sizeof(set_t));
+		if (cache->sets[i] == NULL) return NULL;
+		cache->sets[i]->size = e;
+		for (int j = 0; j < e; j++) {
+			cache->sets[i]->lines[j] = malloc(sizeof(line_t));
+			if (cache->sets[i]->lines[j] == NULL) return NULL;
+		}
+	}
+	return cache;
+	// IMPLEMENTAR LIBERAR MEMORIA DE LOS DEMAS SETS O LINEAS SI ALGUN MALLOC FALLA
 }
-
-/* Valida que el input sea correcto
-	Abre el archivo
-	Crea el cache
-	Llama al simulador
-	Cierra el archivo
-	Libera el cache
-*/
-
 
 int cache_simulator(FILE *tracefile, cache_t *cache) {
 	// Parseo las líneas del archivo
@@ -59,11 +74,11 @@ int cache_simulator(FILE *tracefile, cache_t *cache) {
 	char **strv; // Para guardar los campos de cada línea
 	char *line = NULL; // Para que getline se encargue de manejar la memoria
 	size_t bytes = 0; // Para que getline se encargue de manejar la memoria
-	long int read_bytes;
+	long read_bytes;
 	FILE *input;
 	while ((read_bytes = getline(&line, &bytes, input)) != -1 && !errors) {
 		if (line[read_bytes - 1] == '\n') line[--read_bytes] = '\0'; // Piso el \n
-		strv = split(line, ' '); // Separo la línea por espacios
+		strv = split(line, ' ');
 
 		instruction_pointer = (int) strtol(strv[0], NULL, 16);
 		operation = strv[1][0];
@@ -91,35 +106,44 @@ int is_power_of_2(int x) {
 
 int inputs_are_valid(int a, int b, int c) {
 	if (a <= 0 || b <= 0 || c <= 0) return 0;
-	return is_power_of_2(a) || is_power_of_2(b) || is_power_of_2(c);
+	return is_power_of_2(a) && is_power_of_2(b) && is_power_of_2(c);
 }
 
-int main(int argc, char const *argv[]) {
-	// ./cachesim tracefile.xex C E S [ -v n m ]
+int main(int argc, char const *argv[]) { // ./cachesim tracefile.xex C E S -v n m
+	/*
+	Valida input, abre el archivo, crea el cache, llama al simulador
+	cierra el archivo y libera el cache
+	*/
+
 	if (!(argc == 5 || argc == 8)) {
 		fprintf(stderr, "Error: Cantidad de argumentos incorrecta\n");
 		exit(1);
 	}
-	int c = atoi(argv[2]); // tamaño de caché (bytes)
+
+	int c = atoi(argv[2]); // tamaño de caché en bytes
 	int e = atoi(argv[3]); // líneas por set
 	int s = atoi(argv[4]); // número de sets
+
 	if (inputs_are_valid(c, e, s)) {
 		fprintf(stderr, "Error: Argumento incorrecto\n");
 		exit(1);
 	}
+	bool verbose = false;
 	int n, m;
 	if (argc == 8) {
-		n = atoi(argv[6]); // líneas por set
-		m = atoi(argv[7]); // número de sets
+		if (strcmp(argv[5], "-v") == 0) {
+			n = atoi(argv[6]);
+			m = atoi(argv[7]);
+			verbose = true;
+		}
 		if (n <= 0 || m <= 0 || n > m) {
-			fprintf(stderr, "Error en modo verboso: Argumento incorrecto\n");
+			fprintf(stderr, "Error: Argumento incorrecto\n");
 			exit(1);
 		}
 	}
 
-	FILE *tracefile;
 	const char *input_file = argv[1];
-	tracefile = fopen(input_file, "r");
+	FILE *tracefile = fopen(input_file, "r");
 	if (!tracefile) {
 		fprintf(stderr, "Error: Archivo fuente inaccesible");
 		return 0;
